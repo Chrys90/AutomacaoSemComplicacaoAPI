f634aa1c285195490072e10c006cc2acd0786552
Chrystopher Gois <chrystophergois@hotmail.com>
2024-07-25 18:40:01 -0300

    aula 14 cucumber e steps

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,44 +1,41 @@
 package steps;
 
+import groovy.util.logging.Log;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import io.restassured.http.ContentType;
+import maps.LoginMap;
+import utils.RestUtils;
+
+import java.util.Map;
 
 public class LoginSteps {
+    String url = "http://localhost:8080/";
 
     @Dado("que eu tenha um payload valido de API de Login")
     public void queEuTenhaUmPayloadValidoDeAPIDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        LoginMap.initLogin();
+        RestUtils.setBaseURI(url);
     }
 
     @Entao("armazeno o token que recebo do response do login")
     public void armazenoOTokenQueReceboDoResponseDoLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        LoginMap.token = RestUtils.getResponse().jsonPath().get("token");
     }
 
     @Dado("que eu tenha um payload de Login com as seguintes informacoes")
-    public void queEuTenhaUmPayloadDeLoginComAsSeguintesInformacoes(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void queEuTenhaUmPayloadDeLoginComAsSeguintesInformacoes(Map<String, Object> map) {
+        LoginMap.initLogin();
+        RestUtils.setBaseURI(url);
+        LoginMap.getLogin().putAll(map);
+
     }
     @Quando("envio uma requisicao do tipo POST de login")
     public void envioUmaRequisicaoDoTipoPOSTDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       RestUtils.post(LoginMap.getLogin(), ContentType.JSON,"auth");
     }
-    @Entao("valido que recebo status {int} no response")
-    public void validoQueReceboStatusNoResponse(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
 
 
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,18 +1,29 @@
 #language:pt
-
+@Login
 Funcionalidade: Realizar Login
   Testes de API de login
 
+  @LoginSucesso @regressivo
   Cenario: Realizar login com sucesso
     Dado que eu tenha um payload valido de API de Login
-    Quando envio uma requisicao do tipo POST de Login
+    Quando envio uma requisicao do tipo POST de login
     Entao valido que recebo status 200 no response
     E armazeno o token que recebo do response do login
 
-
-  Cenario: Realizar Login com usuario invalido
+  @LoginInvalido
+  Esquema do Cenario: Realizar Login com <cenario>
     Dado que eu tenha um payload de Login com as seguintes informacoes
-  |email|invalido@email.com|
-  |senha|123456            |
+      | email | <email> |
+      | senha | <senha> |
     Quando envio uma requisicao do tipo POST de login
     Entao valido que recebo status 400 no response
+
+    @LoginUsuarioInvalido @regressivo
+    Exemplos:
+      | cenario          | email              | senha    |
+      | usuario invalido | invalido@email.com | 123456   |
+
+    @LoginSenhaInvalida
+    Exemplos:
+      | cenario          | email              | senha    |
+      | senha invalida   | aluno@email.com    | invalido |
